4e2689f7ca11a36a3aa8184b8f93af50e63e66aa
Léo Barroux <leo.barroux@gmail.com>
2022-05-22 19:16:44 +0200

    PacketFusionInit creation + used

---
+++ src/fr/upem/net/tcp/chatfusion/Packet/PacketFusionInit.java
@@ -0,0 +1,54 @@
+package fr.upem.net.tcp.chatfusion.Packet;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class PacketFusionInit implements Packet {
+    private final int opCode;
+    private final List<String> components;
+    private final String name;
+    private final Charset cs = StandardCharsets.UTF_8;
+
+    public PacketFusionInit(int opCode, String name, int nbMembers, List<String> members) {
+        this.opCode = opCode;
+        this.components = members;
+        this.name = name;
+    }
+
+    @Override
+    public int opCodeGet() {
+        return opCode;
+    }
+
+    @Override
+    public int size() {
+        var size = Integer.BYTES * 2;
+        size += cs.encode(name).limit() + Integer.BYTES;
+        for (var component : components)
+            size += cs.encode(component).limit() + Integer.BYTES;
+        return size;
+    }
+
+    @Override
+    public List<String> components() {
+        return components;
+    }
+
+    @Override
+    public ByteBuffer parseToByteBuffer() {
+        ByteBuffer bb = ByteBuffer.allocate(size());
+        bb.putInt(opCode);
+        var bbName = cs.encode(name);
+        bb.putInt(bbName.limit());
+        bb.put(bbName);
+        bb.putInt(name.length());
+        for (var component : components) {
+            var bbComponent = cs.encode(component);
+            bb.putInt(bbComponent.limit());
+            bb.put(bbComponent);
+        }
+        return bb;
+    }
+}
