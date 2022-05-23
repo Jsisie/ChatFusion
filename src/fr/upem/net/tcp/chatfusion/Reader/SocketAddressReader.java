8786063ebe6f2f2f72e3728343fa143ad8fc5044
Léo Barroux <leo.barroux@gmail.com>
2022-05-23 14:37:53 +0200

    creation SocketAddressReader

---
+++ src/fr/upem/net/tcp/chatfusion/Reader/SocketAddressReader.java
@@ -0,0 +1,23 @@
+package fr.upem.net.tcp.chatfusion.Reader;
+
+import java.nio.ByteBuffer;
+
+public class SocketAddressReader implements Reader {
+
+
+
+    @Override
+    public ProcessStatus process(ByteBuffer bb) {
+        return null;
+    }
+
+    @Override
+    public Object get() {
+        return null;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
