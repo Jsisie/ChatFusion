881b41e641f721f63fd012b8e497128661fdb666
Robin <robin.rieutord@gmail.com>
2022-05-23 23:14:30 +0200

    Socket addressReader done

--- src/fr/upem/net/tcp/chatfusion/Reader/SocketAddressReader.java
+++ src/fr/upem/net/tcp/chatfusion/Reader/SocketAddressReader.java
@@ -1,22 +1,63 @@
 package fr.upem.net.tcp.chatfusion.Reader;
 
+import fr.upem.net.tcp.chatfusion.Packet.Message;
+
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
+import java.util.StringJoiner;
 
 public class SocketAddressReader implements Reader<SocketAddress> {
+    private enum State {DONE, WAITING, ERROR}
+    private int ipv;
+    private StringJoiner sj = new StringJoiner(".");
+    private int port;
+    private InetSocketAddress value;
 
+    private final IntReader intReader = new IntReader();
+    private State state = State.WAITING;
     @Override
     public ProcessStatus process(ByteBuffer bb) {
-        return null;
+        if (state == SocketAddressReader.State.DONE || state == SocketAddressReader.State.ERROR) {
+            throw new IllegalStateException();
+        }
+        var intState = intReader.process(bb);
+
+        if (intState.equals(ProcessStatus.DONE)) {
+            ipv = intReader.get();
+        } else if (intState.equals(ProcessStatus.ERROR)) {
+            state = State.ERROR;
+        }
+        intReader.reset();
+
+        for(int i = 0; i < ipv; i++){
+            var b =  (bb.get() & 0xff) ;
+            sj.add(Integer.toString(b));
+        }
+        intState = intReader.process(bb);
+
+        if (intState.equals(ProcessStatus.DONE)) {
+            port = intReader.get();
+        } else if (intState.equals(ProcessStatus.ERROR)) {
+            state = State.ERROR;
+        }
+
+
+        state = State.DONE;
+        value = new InetSocketAddress(sj.toString(), port);
+        intReader.reset();
+        return ProcessStatus.DONE;
+
     }
 
     @Override
     public SocketAddress get() {
-        return null;
+        return value;
     }
 
     @Override
     public void reset() {
-
+        state = State.WAITING;
+        sj= new StringJoiner(".");
     }
 }
