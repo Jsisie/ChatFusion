8a81e7622c442b3a3766fffcab2fa7221dd6f841
Léo Barroux <leo.barroux@gmail.com>
2022-05-24 00:07:03 +0200

    PacketSocketAddress done

--- src/fr/upem/net/tcp/chatfusion/Packet/PacketSocketAddress.java
+++ src/fr/upem/net/tcp/chatfusion/Packet/PacketSocketAddress.java
@@ -1,14 +1,17 @@
 package fr.upem.net.tcp.chatfusion.Packet;
 
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class PacketSocketAddress implements Packet {
 
     private final int opCode;
-
     private final SocketAddress sa;
+    private final Charset cs = StandardCharsets.UTF_8;
 
     public PacketSocketAddress(int opCode, SocketAddress sa) {
         this.opCode = opCode;
@@ -17,21 +20,29 @@ public class PacketSocketAddress implements Packet {
 
     @Override
     public int opCodeGet() {
-        return 0;
+        return opCode;
     }
 
     @Override
     public int size() {
-        return 0;
+        int size = Integer.BYTES;
+        size += Integer.BYTES * 2 + Byte.BYTES * 4;
+        return size;
     }
 
     @Override
-    public List<?> components() {
-        return null;
+    public List<SocketAddress> components() {
+        return List.of(sa);
     }
 
     @Override
     public ByteBuffer parseToByteBuffer() {
-        return null;
+        var bb = ByteBuffer.allocate(size());
+        bb.putInt(opCode);
+        var inetSA = (InetSocketAddress) sa;
+        var bbIPAddress = cs.encode(inetSA.getHostName());
+        bb.put(bbIPAddress);
+        bb.putInt(inetSA.getPort());
+        return bb;
     }
 }
